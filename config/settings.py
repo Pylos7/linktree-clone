cb925b317d0377d30001b1f230142b4a55fbfec8
Pylos7 <nestor1ingles@gmail.com>
2024-05-05 17:15:14 -0400

    changed templates directory

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'link_plant/templates'],
+        'DIRS': ['link_plant/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
